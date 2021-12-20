7759e1e3b40d43c5f1cfb957d030e63be243dee9
André de Paula Terceiro <andre.terceiro@riachuelo.com.br>
2021-12-20 16:11:26 -0300

    feat: updating logic

--- index.php
+++ index.php
@@ -1,27 +1,35 @@
 <?php
 define('SUCESSO', 1);
-define('FRACASS0', 2);
+define('FRACASSO', 2);
 
 echo "Insira a quantidade de farinha: ";
 $quantidadeFarinha = fgets(STDIN);
 
 while ($quantidadeFarinha != 50) {
-    enviarMensagem("A quantidade de farinha não está certa. Por favor tente novamente", FRACASS0);
+    enviarMensagem("A quantidade de farinha não está certa. Por favor tente novamente", FRACASSO);
     echo "Insira a quantidade de farinha: ";
     $quantidadeFarinha = fgets(STDIN);
 }
 enviarMensagem("Você inseriu a quantidade certa de farinha", SUCESSO);
 
-echo "Insira a quantidade de ovos: ";
-$quantidadeOvos = fgets(STDIN);
-
-while ($quantidadeOvos != 3) {
-    enviarMensagem("A quantidade de ovos não está certa. Por favor tente novamente", FRACASS0);
-    echo "Insira a quantidade de ovos: ";
-    $quantidadeOvos = fgets(STDIN);
+$quantidadeOvos = 0;
+while (true) {
+    echo "Você quer mais um ovo (s/n)? ";
+    $querMaisOvo = trim(strtoupper(fgets(STDIN)));
+    if ($querMaisOvo == "S") {
+        $quantidadeOvos++;
+    } elseif ($querMaisOvo != "N") {
+        enviarMensagem("Você deve escrever sim (S) ou não (N)", FRACASSO);
+    } else {
+        break;
+    }
 }
-enviarMensagem("Você inseriu a quantidade certa de ovos", SUCESSO);
 
+if ($quantidadeOvos != 3) {
+    enviarMensagem("A quantidade de ovos não está certa.", FRACASSO);
+} else {
+    enviarMensagem("Você inseriu a quantidade certa de ovos", SUCESSO);
+}
 
 function enviarMensagem($mensagem, $status) {    
     if ($status == SUCESSO) {
